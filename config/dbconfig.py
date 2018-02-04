1b9b3a303155f15288d602289141f6d62dfa0cc0
Shubham Sharma <shhubhamsharma@gmail.com>
2018-02-04 19:25:38 +0530

    Update dbconfig.py

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-dbConfig = {'host': '192.99.149.208',
-         'user': 'shubhams_sav',
-         'passwd': '68MDQ3xz9iRE',
-         'db': 'shubhams_Task'}
+dbConfig = {'host': 'hostname',
+         'user': 'username',
+         'passwd': 'passwd',
+         'db': 'dbname'}
 
 # use_anonymous = True
